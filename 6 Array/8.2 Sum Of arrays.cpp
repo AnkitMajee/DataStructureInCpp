8d00999e46d1062c2760d1ff52b514c3a19a0cf3
Ankit Majee <76817118+AnkitMajee@users.noreply.github.com>
2023-01-20 20:57:28 +0530

    Basic to advance

---
+++ 6 Array/8.2 Sum Of arrays.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n,sum=0;
+	cin>>n;
+	int arr[n];
+	for(int i=0; i<n;i++){
+		cin>>arr[i];
+	}
+	for(int i=0; i<n;i++){
+		sum+=arr[i];
+	}
+cout<<"Sum="<<sum<<" "<<endl;
+	
+	
+return 0;
+}
+
---
+++ 6 Array/8.3 Greater NUmber.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	 int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int maxNO =0;
+    
+    for(int i=0;i<n;i++){
+        maxNO = max(maxNO, arr[i]);
+		}
+        
+cout<<maxNO<<endl;
+
+return 0;
+}
+
