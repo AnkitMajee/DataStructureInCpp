40acc9d049205bcfe9fae6aa0c90bd56aae303ea
Ankit Majee <76817118+AnkitMajee@users.noreply.github.com>
2023-01-23 21:56:34 +0530

    String Basic

---
+++ 9 String/10.Convert Upper Case to Lower case.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+#include<math.h>
+#include<climits>
+#include<algorithm>
+#include<stdio.h>
+#include<conio.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+	string s="xfgt   ADADDS";
+	string s1="ASDFF  fhgfh";
+	//upper case
+/*	for(int i=0; i<s.size();i++)
+	{
+		if(s[i]>='a'|| s[i]<='z')
+		s[i]-=32;
+	}
+	//lower case
+	for(int i=0; i<s1.size();i++)
+	{
+		if(s1[i]>='A'|| s[i]<='Z')
+		s1[i]+=32;
+	}
+	cout<<s<<endl;
+	cout<<s1<<endl;*/
+	
+	
+transform(s1.begin(),s1.end(),s1.begin(),::toupper);
+cout<<s1<<endl;
+transform(s.begin(),s.end(),s.begin(),::tolower);
+cout<<s<<endl;
+
+
+
+
+return 0;
+}
+
+
---
+++ 9 String/11.Form the biggest number from the gicen number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<math.h>
+#include<climits>
+#include<algorithm>
+#include<stdio.h>
+#include<conio.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+string s="88237830090";
+sort(s.begin(),s.end(),greater<int>());
+cout<<s<<endl;
+return 0;
+}
+
+
