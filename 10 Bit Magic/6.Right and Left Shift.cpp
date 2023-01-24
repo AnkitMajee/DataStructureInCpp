a19a7bbbe659e856f8f51b18d5cb601e4937375b
Ankit Majee <76817118+AnkitMajee@users.noreply.github.com>
2023-01-24 21:56:00 +0530

    Add files via upload

---
+++ 10 Bit Magic/6.Right and Left Shift.cpp
@@ -0,0 +1,18 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+	int x=33;
+	// Rigth Shift >>
+	cout<<(x>>1)<<"\n";
+	cout<<(x>>2)<<"\n";
+	
+	// Left Shift <<
+	cout<<(x<<1)<<"\n";
+	cout<<(x<<2)<<"\n";
+	
+	
+return 0;
+}
+
