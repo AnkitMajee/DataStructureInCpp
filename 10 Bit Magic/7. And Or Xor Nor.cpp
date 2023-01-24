daf1d436edfadc47317240cd504ce1e156c1678e
Ankit Majee <76817118+AnkitMajee@users.noreply.github.com>
2023-01-24 21:56:33 +0530

    Add files via upload

---
+++ 10 Bit Magic/7. And Or Xor Nor.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+	int x=3,y=6;
+	// AND of two Numbers &
+	cout<<"AND ="<<(x&y)<<endl;
+	
+	// OR of two Number |
+	cout<<"OR ="<<(x|y)<<endl;
+	
+	// XOR of two Numbers ^
+	cout<<"XOR ="<<(x^y)<<endl;
+	
+	// NOR of x and y ~
+	cout<<"NOR ="<<(~y)<<endl;
+	cout<<"NOR ="<<(~x)<<endl;
+	
+	unsigned int z=1;
+	cout<<(~z)<<"\n";
+	z=5;
+	cout<<(~z)<<"\n";
+	
+	int n=1;
+	cout<<(~n)<<"\n";
+	n=5;
+	cout<<(~n)<<"\n";
+return 0;
+}
+
