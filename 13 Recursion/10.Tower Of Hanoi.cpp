21542390274bf148f5a271ad617d6f667340497d
Ankit Majee <76817118+AnkitMajee@users.noreply.github.com>
2023-01-27 19:06:25 +0530

    Tower Of Hanoi

---
+++ 13 Recursion/10.Tower Of Hanoi.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<math.h>
+#include<climits>
+#include<algorithm>
+#include<bits/stdc++.h>
+//Goto Tools > Editor Option > Snippets > Default Source Code
+
+using namespace std;
+void towerofhanoi(int n,char src,char dest,char helper){
+	if(n==0){
+		return;
+	}
+	towerofhanoi(n-1,src,helper,dest);
+	cout<<"Move From "<<src <<" to "<< dest<<endl;
+	towerofhanoi(n-1,helper,dest,src);
+}
+int main(){
+	towerofhanoi(3,'A','C','B');
+return 0;
+}
+
+
