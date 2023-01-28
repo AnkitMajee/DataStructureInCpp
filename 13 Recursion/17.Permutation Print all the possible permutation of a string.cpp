923e1f7a2a2aa63d719f6e9bc80459001d86160d
Ankit Majee <76817118+AnkitMajee@users.noreply.github.com>
2023-01-28 22:38:05 +0530

    Permutation Print all the possible permutation of a string

---
+++ 13 Recursion/17.Permutation Print all the possible permutation of a string.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+#include<math.h>
+#include<climits>
+#include<algorithm>
+#include<bits/stdc++.h>
+//Goto Tools > Editor Option > Snippets > Default Source Code
+
+using namespace std;
+void permutation(string s,string ans){
+	if(s.length()==0){
+	cout<<ans<<endl;
+	return;
+}
+for(int i=0;i<s.length();i++){
+	char ch=s[i];
+	cout<<ch<<endl;
+	cout<<"##############"<<endl;
+	string ros=s.substr(0,i)+s.substr(i+1);
+	cout<<ros<<endl;
+	cout<<"***************"<<endl;
+
+	permutation(ros,ans+ch);
+}
+}
+
+int main(){
+	permutation("ABC"," ");
+return 0;
+}
+
+
