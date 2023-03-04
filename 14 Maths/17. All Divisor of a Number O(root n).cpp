2c5a4001f46813604a5d513d200c4c83ab90819a
Ankit Majee <76817118+AnkitMajee@users.noreply.github.com>
2023-03-04 23:02:50 +0530

    Add files via upload

---
+++ 14 Maths/17. All Divisor of a Number O(root n).cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+int printDivisor(int n){
+	for(int i=1;i*i<=n;i++)
+	if(n%i ==0){
+		cout<<i<<" ";
+		if(i!=n/i)
+		cout<<n/i<<" ";
+	}
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin>>n;
+  
+  return printDivisor(n);  
+}
+
+
+
+
+
+
+//Goto tools > Editor Option > Snippets >Default Source Code
+
