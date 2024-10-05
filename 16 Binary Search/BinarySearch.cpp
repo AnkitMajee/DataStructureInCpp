a06c2f10092345e939527e6ec2750e55706d0e40
Arijit Ghosh <ajghosh017@gmail.com>
2024-10-06 01:25:46 +0530

    Add binary search problems

---
+++ 16 Binary Search/BinarySearch.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int binarySearch(vector<int>& arr,int left,int right,int target){
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        if(arr[mid]<target){
+            left=mid+1;
+        } else {
+            right=mid-1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int n,target;
+    cout<<"Enter the number of elements in the array: ";
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter the elements of the array:\n";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr.begin(),arr.end());
+    cout<<"Enter the element to search: ";
+    cin>>target;
+    int result=binarySearch(arr,0,n-1,target);
+    if(result!=-1){
+        cout<<"Element found at index: "<<result<<endl;
+    } else {
+        cout<<"Element not found in the array\n";
+    }
+    return 0;
+}
---
+++ 16 Binary Search/minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMin(vector<int>& arr) {
+    int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[low] <= arr[high]) {
+            ans = min(ans, arr[low]);
+            break;
+        }
+        if (arr[low] <= arr[mid]) {
+            ans = min(ans, arr[low]);
+            low = mid + 1;
+        }
+        else {
+            ans = min(ans, arr[mid]);
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {4, 5, 6, 7, 0, 1, 2, 3};
+    int ans = findMin(arr);
+    cout << "The minimum element is: " << ans << "\n";
+    return 0;
+}
+
